8235d68eea27dcb0474795c3118d345a312e1c29
AlvinAhmadov <alvin.dev.ahmadov@gmail.com>
2021-01-15 12:51:31 +0400

    Update test values

--- yowsup/layers/coder/test_tokendictionary.py
+++ yowsup/layers/coder/test_tokendictionary.py
@@ -1,20 +1,22 @@
 from yowsup.layers.coder.tokendictionary import TokenDictionary
 import unittest
+
+
 class TokenDictionaryTest(unittest.TestCase):
     def setUp(self):
         self.tokenDictionary = TokenDictionary()
 
     def test_getToken(self):
-        self.assertEqual(self.tokenDictionary.getToken(80), "iq")
+        self.assertEqual(self.tokenDictionary.getToken(10), "iq")
 
     def test_getIndex(self):
-        self.assertEqual(self.tokenDictionary.getIndex("iq"), (80, False))
+        self.assertEqual(self.tokenDictionary.getIndex("iq"), (10, False))
 
     def test_getSecondaryToken(self):
-        self.assertEqual(self.tokenDictionary.getToken(238), "amrnb")
+        self.assertEqual(self.tokenDictionary.getToken(238), "lc")
 
     def test_getSecondaryTokenExplicit(self):
-        self.assertEqual(self.tokenDictionary.getToken(11, True), "wmv")
+        self.assertEqual(self.tokenDictionary.getToken(11, True), "reject")
 
     def test_getSecondaryIndex(self):
-        self.assertEqual(self.tokenDictionary.getIndex("wmv"), (11, True))
+        self.assertEqual(self.tokenDictionary.getIndex("reject"), (11, True))
